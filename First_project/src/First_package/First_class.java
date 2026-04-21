edfb6dbdc9d61169796e5dbbac7b95bbd46ed6ab
TAPABRATA SIKDER <133255260+TapabrataSikder@users.noreply.github.com>
2026-04-21 10:51:49 +0530

    Revert "test branch"
    This reverts commit 4ae647907c9652eaa36d7d3c32b3d661dc87dfe1.

--- First_project/src/First_package/First_class.java
+++ First_project/src/First_package/First_class.java
@@ -4,8 +4,7 @@ public class First_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First Class");
-		System.out.print("Hello");
+		System.out.print("HEllo");
 	}
 
 }
